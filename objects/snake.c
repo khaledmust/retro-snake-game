c6a075060323b7607b00f0f1fbea9a90880539f9
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-09-19 12:49:50 +0300

    Add comment documentation to the snake game object.

--- objects/snake.c
+++ objects/snake.c
@@ -1,21 +1,60 @@
 #include "snake.h"
-#include "deque.h"
-#include "raylib.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @var SnakeDirection[]
+ * @brief An array of Vector2 structures representing snake movement directions.
+ *
+ * The SnakeDirection array contains four Vector2 structures, each representing
+ * one of the possible directions in which a snake can move: up, down, right,
+ * and left. */
 Vector2 SnakeDirection[4] = {
-    {.x = 0, .y = -1}, {.x = 0, .y = 1}, {.x = 1, .y = 0}, {.x = -1, .y = 0}};
+    {.x = 0, .y = -1}, /**< Represents the up direction (0, -1). */
+    {.x = 0, .y = 1},  /**< Represents the down direction (0, 1). */
+    {.x = 1, .y = 0},  /**< Represents the right direction (1, 0). */
+    {.x = -1, .y = 0}  /**< Represents the left direction (-1, 0). */
+};
 
-void Snake_AppendTail(Snake *self, void *VectorCordinates) {
-  pushBack(self->snake, VectorCordinates);
+/**
+ * @brief The function appends a new node to the tail of the snake.
+ *
+ * The function appends a new node to the snake's body, which includes the
+ * coordinates of it. This opertaion is accomplished using the pushBack() from
+ * the deque.h library.
+ *
+ * @param self A pointer to the Snake object to which the tail will be appended.
+ * @param VectorCoordinates A pointer to the Vector coordinates of the new tail
+ * element. */
+void Snake_AppendTail(Snake *self, void *VectorCoordinates) {
+  pushBack(self->snake, VectorCoordinates);
 }
 
+/**
+ * @brief The function appends a new node to the head of the snake.
+ *
+ * The function appends a new node to the snak's body, which includes the
+ * coordinates of it. This operation is accomplished using the pushFront() from
+ * the deque.h library.
+ *
+ * @param self A pointer to the Snake object to which the head will be appended.
+ * @param VectorCoordinates A pointer to the Vector coordinates of the new head.
+ */
 void Snake_AppendHead(Snake *self, void *VectorCoordinates) {
   pushFront(self->snake, VectorCoordinates);
 }
 
+/**
+ * @brief Draws the snake object based on the coordinates of each node.
+ *
+ * This function iterates through every node in the snake's body and draws it
+ * according to its coordinates. The drawing operation utilizes the
+ * `DrawRectangleRounded()` function.
+ *
+ * @param self A pointer to the Snake object to be iterated over and drawn.
+ * @param CellSize The size of the cell occupied by each snake node.
+ * @param color The color used for rendering the snake. */
 void GameObject_Draw_Snake(Snake *self, int CellSize, Color color) {
   t_deque_node *current_node = self->snake->first;
   printf("The coordinates of the current node are x: %f, y: %f.\n",
@@ -32,10 +71,21 @@ void GameObject_Draw_Snake(Snake *self, int CellSize, Color color) {
   }
 }
 
+/** @brief Updates the Snake game object to move according to its current
+ * direction.
+ *
+ * This function is responsible for controlling the motion of the Snake game
+ * object. It updates the coordinates of every node in the snake's body based on
+ * the current direction of motion, effectively moving the snake in its intended
+ * direction.
+ *
+ * @param self A pointer to the Snake game object to be updated.
+ */
 void GameObject_Snake_Update(struct object_snake *self) {
   printf("This is the update function\n");
-  Vector2 *HeadCoordinate = (Vector2*)peekFront(self->snake);
-  printf("The content of the head is %f %f\n", HeadCoordinate->x, HeadCoordinate->y);
+  Vector2 *HeadCoordinate = (Vector2 *)peekFront(self->snake);
+  printf("The content of the head is %f %f\n", HeadCoordinate->x,
+         HeadCoordinate->y);
   Vector2 *NewCoordinates = malloc(sizeof(Vector2));
   *NewCoordinates = Vector2Add(*HeadCoordinate, self->direction);
   printf("The new coordinates are x: %f, y: %f.\n", NewCoordinates->x,
@@ -45,6 +95,7 @@ void GameObject_Snake_Update(struct object_snake *self) {
   printf("Exit snake update.\n");
 }
 
+// THIS FUNCTION SHOULD BELONG TO THE GAME CLASS.
 bool GameObject_Snake_SetSpeed(struct object_snake *self) {
   double current_time = GetTime();
   /* Calculate the Delta Time and compare it to the speicfied interval (speed).
@@ -57,6 +108,18 @@ bool GameObject_Snake_SetSpeed(struct object_snake *self) {
   }
 }
 
+/**
+ * @brief Deinitializes and cleans up the Snake game object.
+ *
+ * This function is responsible for the deinitialization and cleanup of the
+ * Snake game object. It frees all memory associated with the snake's body
+ * nodes, using the popBack().
+ * This is done by iterating over the snake's body starting from its tail and
+ * freeing up its contents.
+ *
+ * \TODO We still need to free up the node itself rather than only its content.
+ *
+ * @param self A pointer to the Snake game object to be deinitialized. */
 void Snake_DeInit(Snake *self) {
   t_deque_node *current_node = self->snake->last;
   while (current_node != NULL) {
@@ -65,17 +128,34 @@ void Snake_DeInit(Snake *self) {
   }
 }
 
+/**
+ * @brief Initializes a Snake game object.
+ *
+ * @param self A pointer to the Snake game object to be initialized. */
 void Snake_Init(Snake *self) {
   printf("Enter snake Init.\n");
+  /* Initialize the deque for the snake's body. */
   self->snake = dequeInit();
+
+  /* Assign function pointers for appending to the tail and head. */
   self->AppendToTail = Snake_AppendTail;
   self->AppendToHead = Snake_AppendHead;
+
+  /* Set the initial state for adding a new segment. */
   self->add_segment = false;
+
+  /* Assign the drawing and updating functions. */
   self->Draw = GameObject_Draw_Snake;
   self->Update = GameObject_Snake_Update;
+
+  /* Initialize the last update time and speed. */
   self->last_update_time = 0;
   self->speed = 0.2;
+
+  /* Assign a function for setting the snake's speed. */
   self->SetSpeed = GameObject_Snake_SetSpeed;
+
+  /* Assign a function for deinitializing the snake object. */
   self->SnakeDeInit = Snake_DeInit;
   printf("Exit snake init.\n");
 }
--- objects/snake.h
+++ objects/snake.h
@@ -1,30 +1,51 @@
 #ifndef SNAKE_H_
 #define SNAKE_H_
 
+#include "deque.h"
 #include "raylib.h"
 #include "raymath.h"
-#include "deque.h"
 
+/**
+ * @enum Directions
+ * @brief Represents the possible directions a snake can move. */
 typedef enum { NORTH, SOUTH, EAST, WEST } Directions;
 
 extern Vector2 SnakeDirection[4];
 
-typedef struct object_snake{
-    t_deque *snake;
-    Vector2 direction;
-    bool add_segment;
-    double speed;
-    double last_update_time;
-    void (*AppendToTail) (struct object_snake *self, void *VectorCordinates);
-    void (*AppendToHead) (struct object_snake *self, void *VectorCoordinates);
-    void (*Draw) (struct object_snake *self, int CellSize, Color color);
-    void (*Update) (struct object_snake *self);
-    bool (*SetSpeed) (struct object_snake *self);
-    void (*SnakeDeInit) (struct object_snake *self);
-}Snake;
+/**
+ * @struct Snake
+ * @brief Represents a Snake game object.
+ *
+ * The Snake structure defines the properties and functions associated with a
+ * Snake game object. It includes the snake's body represented as a deque, the
+ * current direction of motion, a flag to indicate whether to add a new segment,
+ * the snake's speed, the last update time, and function pointers for various
+ * snake-related operations like appending to the tail or head, drawing,
+ * updating, setting speed, and deinitialization.
+ */
+typedef struct object_snake {
+  t_deque *snake;          /**< The deque representing the snake's body. */
+  Vector2 direction;       /**< The current direction of motion. */
+  bool add_segment;        /**< Flag indicating whether to add a new segment. */
+  double speed;            /**< The snake's speed. */
+  double last_update_time; /**< The timestamp of the last update. */
+  void (*AppendToTail)(struct object_snake *self,
+                       void *VectorCordinates); /**< Function pointer for
+                                                   appending to the tail. */
+  void (*AppendToHead)(struct object_snake *self,
+                       void *VectorCoordinates); /**< Function pointer for
+                                                    appending to the head. */
+  void (*Draw)(struct object_snake *self, int CellSize,
+               Color color); /**< Function pointer for drawing the snake. */
+  void (*Update)(struct object_snake
+                     *self); /**< Function pointer for updating the snake. */
+  bool (*SetSpeed)(struct object_snake *self); /**< Function pointer for setting
+                                                  the snake's speed. */
+  void (*SnakeDeInit)(
+      struct object_snake
+          *self); /**< Function pointer for deinitializing the snake. */
+} Snake;
 
 void Snake_Init(Snake *self);
-void Snake_AppendTail(Snake *self, void *VectorCordinates);
-
 
 #endif // SNAKE_H_
